5e3d40a63dd267f5dd29a31976f4cf11b2dfc3e1
shivaraj221 <139696646+shivaraj221@users.noreply.github.com>
2025-12-19 10:46:35 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -164,11 +164,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
